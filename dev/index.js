7bfa089639344e3279b4889c47bc3a25a59cd00e
sjpgarcia <91515796+sjpgarcia@users.noreply.github.com>
2021-12-27 09:28:18 +0000

    Fix hot reload and tags

--- dev/index.js
+++ dev/index.js
@@ -1,2 +1,18 @@
-let Main = require('../output/Main');
-Main.main();
+let app
+
+function main() {
+    app = require("../output/Main/index.js").main()
+}
+
+if (module.hot) {
+  module.hot.accept(function() {
+    app != null && typeof app.kill === "function" && app.kill()
+    Array.prototype.forEach.call(
+      document.querySelectorAll("#root>div"),
+      function(d) { d.remove() },
+    )
+    main()
+  })
+}
+
+main()
