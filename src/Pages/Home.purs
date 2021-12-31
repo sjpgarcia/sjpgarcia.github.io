d59170b451ffb9cda90052eca80283ee3265eb87
sjpgarcia <91515796+sjpgarcia@users.noreply.github.com>
2021-12-31 13:42:19 +0800

    Remove multiline string

--- src/Pages/Home.purs
+++ src/Pages/Home.purs
@@ -54,11 +54,8 @@ component =
           , hyperlink "sjpgarcia" "https://github.com/sjpgarcia"
           , HH.br_
           , HH.br_
-          , HH.text
-              """
-              I'm not actively looking for career opportunities in the
-              functional programming space, although offers are appreciated.
-              """
+          , HH.text "I'm not actively looking for career opportunities in the "
+          , HH.text "functional programming space, although offers are appreciated."
           ]
       , HH.slot_ (Proxy ∷ _ "footer") unit Footer.component unit
       ]
